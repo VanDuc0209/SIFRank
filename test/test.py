80a558a14581ff3ef1bfe4d9df6f19151f46715d
duccv0209 <caovanduca2@gmail.com>
2020-04-16 18:02:31 +0700

    add path sys

--- test/test.py
+++ test/test.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 # __author__ = "Sponge_sy"
 # Date: 2020/2/21
-
+import sys
+sys.path.append('.')
+sys.path.append('..')
 import nltk
 from embeddings import sent_emb_sif, word_emb_elmo
 from model.method import SIFRank, SIFRank_plus
@@ -16,7 +18,7 @@ weight_file = "../auxiliary_data/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
 porter = nltk.PorterStemmer()
 ELMO = word_emb_elmo.WordEmbeddings(options_file, weight_file, cuda_device=0)
 SIF = sent_emb_sif.SentEmbeddings(ELMO, lamda=1.0)
-en_model = StanfordCoreNLP(r'E:\Python_Files\stanford-corenlp-full-2018-02-27',quiet=True)#download from https://stanfordnlp.github.io/CoreNLP/
+en_model = StanfordCoreNLP('../stanford-corenlp-full-2018-10-05',quiet=True)#download from https://stanfordnlp.github.io/CoreNLP/
 elmo_layers_weight = [0.0, 1.0, 0.0]
 
 text = "Discrete output feedback sliding mode control of second order systems - a moving switching line approach The sliding mode control systems (SMCS) for which the switching variable is designed independent of the initial conditions are known to be sensitive to parameter variations and extraneous disturbances during the reaching phase. For second order systems this drawback is eliminated by using the moving switching line technique where the switching line is initially designed to pass the initial conditions and is subsequently moved towards a predetermined switching line. In this paper, we make use of the above idea of moving switching line together with the reaching law approach to design a discrete output feedback sliding mode control. The main contributions of this work are such that we do not require to use system states as it makes use of only the output samples for designing the controller. and by using the moving switching line a low sensitivity system is obtained through shortening the reaching phase. Simulation results show that the fast output sampling feedback guarantees sliding motion similar to that obtained using state feedback"
